55afcee31a0d299456037f38f8e7e1b788f76cac
Filipe Goncalves <filipegoncalves.dev@gmail.com>
2024-11-29 13:49:35 +0000

    .

---
+++ read_json.py
@@ -0,0 +1,19 @@
+import json
+
+try:
+    path_input = input()
+    file = open(path_input, "rt", encoding="utf-8")
+
+    json_data = file.read()
+    data = json.loads(json_data)
+
+    print(data)
+
+
+    file.close()
+except:
+
+    print("Ocorreu um erro!")
+
+finally:
+    print("Processo concluído!")
